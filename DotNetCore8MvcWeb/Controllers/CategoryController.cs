3f946768d09961c63e4f2f587dfba367bba689b7
dwkim <kimdaewoong@kakao.com>
2024-01-16 15:00:26 +0900

    CategoryController, View 추가

---
+++ DotNetCore8MvcWeb/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore8MvcWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
