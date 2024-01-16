cf7f361dd936c652ae41f0180d7dacbafca7e83a
dwkim <kimdaewoong@kakao.com>
2024-01-16 15:04:59 +0900

    테이블 기본 데이터 입력, modelBuilder.Entity<Category>().HasData(

--- DotNetCore8MvcWeb/Migrations/20240116060340_SeedCategoryTableToDb.Designer.cs
+++ DotNetCore8MvcWeb/Migrations/20240116060340_SeedCategoryTableToDb.Designer.cs
@@ -3,6 +3,7 @@ using DotNetCore8MvcWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DotNetCore8MvcWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240116060340_SeedCategoryTableToDb")]
+    partial class SeedCategoryTableToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
