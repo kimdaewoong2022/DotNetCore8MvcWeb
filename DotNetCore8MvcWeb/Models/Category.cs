0c4a9e82cd01fcd3c3addf9eaa16bfb445480434
dwkim <kimdaewoong@kakao.com>
2024-01-16 15:24:08 +0900

    카테고리 데이터 입력

--- DotNetCore8MvcWeb/Models/Category.cs
+++ DotNetCore8MvcWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace DotNetCore8MvcWeb.Models
 {
@@ -7,7 +8,9 @@ namespace DotNetCore8MvcWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("카테고리명")]
         public string Name { get; set; }
+        [DisplayName("순번")]
         public int DisplayOrder { get; set; }
     }
 }
