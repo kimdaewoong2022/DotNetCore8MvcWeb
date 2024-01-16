f9d44affeabc7b939316913e8fb06a5c3f839f13
dwkim <kimdaewoong@kakao.com>
2024-01-16 14:25:42 +0900

    초기 커밋.

---
+++ DotNetCore8MvcWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetCore8MvcWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
