fc97eeb095c7d3008c551b889bccee471ce4983d
dwkim <kimdaewoong@kakao.com>
2024-01-16 15:07:28 +0900

    Category 화면 표시

--- DotNetCore8MvcWeb/Controllers/CategoryController.cs
+++ DotNetCore8MvcWeb/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DotNetCore8MvcWeb.Data;
+using DotNetCore8MvcWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetCore8MvcWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
