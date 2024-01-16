196c84d1018938c32a9a03235d19293d9234dedc
dwkim <kimdaewoong@kakao.com>
2024-01-16 14:37:37 +0900

    DbContext 추가

---
+++ DotNetCore8MvcWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCore8MvcWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- DotNetCore8MvcWeb/Program.cs
+++ DotNetCore8MvcWeb/Program.cs
@@ -1,7 +1,13 @@
+using DotNetCore8MvcWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//Sql Server 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
