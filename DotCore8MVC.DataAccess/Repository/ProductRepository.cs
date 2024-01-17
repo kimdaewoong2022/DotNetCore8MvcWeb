66ff3f37d70395b0189eea95943937b922d83626
dwkim <kimdaewoong@kakao.com>
2024-01-17 13:51:35 +0900

    ProductRepository 구현

---
+++ DotCore8MVC.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using DotCore8MVC.DataAccess.Data;
+using DotCore8MVC.DataAccess.Repository.IRepository;
+using DotCore8MVC.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotCore8MVC.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
