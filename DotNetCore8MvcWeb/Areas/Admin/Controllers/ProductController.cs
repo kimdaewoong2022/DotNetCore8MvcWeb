597b7b7f64e8eb2392923ff875b060f6ec1859df
dwkim <kimdaewoong@kakao.com>
2024-01-18 15:31:23 +0900

    CategoryID -> 카테고리명

--- DotNetCore8MvcWeb/Areas/Admin/Controllers/ProductController.cs
+++ DotNetCore8MvcWeb/Areas/Admin/Controllers/ProductController.cs
@@ -21,13 +21,15 @@ namespace DotNetCore8MvcWeb.Areas.Admin.Controllers
         }
         public IActionResult Index()
         {
-            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
-            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category
-                .GetAll().Select(u => new SelectListItem
-            {
-                Text = u.Name,
-                Value = u.Id.ToString()
-            });
+            //List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            //IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category
+            //    .GetAll().Select(u => new SelectListItem
+            //{
+            //    Text = u.Name,
+            //    Value = u.Id.ToString()
+            //});
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+
             return View(objProductList);
         }
 
