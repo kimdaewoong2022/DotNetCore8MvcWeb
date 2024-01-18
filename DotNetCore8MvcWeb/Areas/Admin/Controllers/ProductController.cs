4400f2203fb7982d26fd37a07b6cbadd0eff680c
dwkim <kimdaewoong@kakao.com>
2024-01-18 09:47:48 +0900

    ViewBag -> ViewData

--- DotNetCore8MvcWeb/Areas/Admin/Controllers/ProductController.cs
+++ DotNetCore8MvcWeb/Areas/Admin/Controllers/ProductController.cs
@@ -35,7 +35,8 @@ namespace DotNetCore8MvcWeb.Areas.Admin.Controllers
                    Value = u.Id.ToString()
                });
 
-            ViewBag.CategoryList = CategoryList;
+            //ViewBag.CategoryList = CategoryList;
+            ViewData["CategoryList"] = CategoryList;
 
             return View();
         }
