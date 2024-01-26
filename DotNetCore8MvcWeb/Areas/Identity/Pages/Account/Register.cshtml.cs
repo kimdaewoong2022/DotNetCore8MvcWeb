7dbc8dffd4cdc3a4b8bedd82854a8299084c1962
dwkim <kimdaewoong@kakao.com>
2024-01-26 15:52:25 +0900

    컴퍼니 등록 추가

--- DotNetCore8MvcWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DotNetCore8MvcWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -173,6 +173,11 @@ namespace DotNetCore8MvcWeb.Areas.Identity.Pages.Account
                 user.PostalCode = Input.PostalCode;
                 user.PhoneNumber = Input.PhoneNumber;
 
+                if (Input.Role == SD.Role_Company)
+                {
+                    user.CompanyId = Input.CompanyId;
+                }
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
