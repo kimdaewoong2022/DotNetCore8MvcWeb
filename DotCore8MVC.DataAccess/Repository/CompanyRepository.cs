7db3fde994198f3b53558d275dd878a526d7e999
dwkim <kimdaewoong@kakao.com>
2024-01-26 15:25:35 +0900

    Company 컨트롤러, 뷰, 모델 추가 Company 테이블 생성   PM> add-migration addCompanyTables   PM> update-database

---
+++ DotCore8MVC.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,27 @@
+﻿using DotCore8MVC.DataAccess.Data;
+using DotCore8MVC.DataAccess.Repository.IRepository;
+using DotCore8MVC.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotCore8MVC.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private ApplicationDbContext _db;
+        public CompanyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+
+        public void Update(Company obj)
+        {
+            _db.Companies.Update(obj);
+        }
+    }
+}
