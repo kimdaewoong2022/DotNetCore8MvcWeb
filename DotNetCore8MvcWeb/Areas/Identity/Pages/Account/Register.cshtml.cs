39b5afd82221b4aa5ba7aa55b1c9ef68163a3df1
dwkim <kimdaewoong@kakao.com>
2024-01-26 15:45:31 +0900

    ApplicationUser 수정   PM> add-migration addCompanyToUser   PM> update-database

--- DotNetCore8MvcWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DotNetCore8MvcWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using DotCore8MVC.DataAccess.Repository.IRepository;
 using DotCore8MVC.Models.Models;
 using DotCore8MVC.Utility;
 using Microsoft.AspNetCore.Authentication;
@@ -34,6 +35,7 @@ namespace DotNetCore8MvcWeb.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly IUnitOfWork _unitOfWork;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
@@ -41,9 +43,12 @@ namespace DotNetCore8MvcWeb.Areas.Identity.Pages.Account
 			IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            //IEmailSender emailSender)
+            IEmailSender emailSender,
+            IUnitOfWork unitOfWork)
         {
-			_roleManager = roleManager;
+            _unitOfWork = unitOfWork;
+            _roleManager = roleManager;
 			_userManager = userManager;
             _userStore = userStore;
             _emailStore = GetEmailStore();
@@ -116,6 +121,10 @@ namespace DotNetCore8MvcWeb.Areas.Identity.Pages.Account
             public string? State { get; set; }
             public string? PostalCode { get; set; }
             public string? PhoneNumber { get; set; }
+
+            public int? CompanyId { get; set; }
+            [ValidateNever]
+            public IEnumerable<SelectListItem> CompanyList { get; set; }
         }
 
 
@@ -135,6 +144,11 @@ namespace DotNetCore8MvcWeb.Areas.Identity.Pages.Account
                 {
                     Text = i,
                     Value = i
+                }),
+                CompanyList = _unitOfWork.Company.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
                 })
             };
 
