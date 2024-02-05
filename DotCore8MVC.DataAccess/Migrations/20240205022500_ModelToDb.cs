5a78b18e7720660820259e6edff7f58223c7e7c1
dwkim <kimdaewoong@kakao.com>
2024-02-05 11:31:12 +0900

    DBConnection 변경, update-database

---
+++ DotCore8MVC.DataAccess/Migrations/20240205022500_ModelToDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotCore8MVC.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
